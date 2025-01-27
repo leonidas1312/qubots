748702fdb9100966c255c92d1c74cafdbea00bef
Ioannis Leonidas <49210924+leonidas1312@users.noreply.github.com>
2025-01-27 20:46:53 +0200

    Update __init__.py

--- rastion_core/__init__.py
+++ rastion_core/__init__.py
@@ -1,1 +1,8 @@
+# rastion_core/__init__.py
+
+# Optionally re-export major classes/functions at the package level:
+# from .base_problem import BaseProblem
+# from .problems.traveling_salesman import TSPProblem
+# from .algorithms.genetic_algorithm import GeneticAlgorithm
+# from .algorithms.gradient_descent import GradientDescent
 
