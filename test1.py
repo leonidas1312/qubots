a7c3b10af4df1e69b87f2515729a3b466c771a20
leonidas1312 <jonhkarystos@gmail.com>
2025-02-13 13:50:42 +0200

    a

--- test1.py
+++ test1.py
@@ -1,12 +1,12 @@
 from qubots.auto_problem import AutoProblem
 from qubots.auto_optimizer import AutoOptimizer
 
-problem = AutoProblem.from_repo(f"Rastion/portfolio-optimization", revision="main")
-optimizer = AutoOptimizer.from_repo(f"Rastion/particle-swarm",
+problem = AutoProblem.from_repo(f"Rastion/portfolio-optimization-with-yf", revision="main", override_params={"tickers":['AAPL', 'MSFT', 'GOOG']})
+optimizer = AutoOptimizer.from_repo(f"Rastion/quantum-qaoa",
                                     revision="main",
-                                    override_params={"swarm_size":60,"max_iters":500})
+                                    override_params={"num_layers":2,"max_iters":100})
 
 best_solution, best_cost = optimizer.optimize(problem)
-print("Portfolio Optimization with PSO")
+print("Portfolio Optimization with QAOA")
 print("Best Solution:", best_solution)
 print("Best Cost:", best_cost)
